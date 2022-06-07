8e519ce39833120f87a5052e2c3832d63d15893b
Emmanuel SARPEDON <emmanuel.sarpedon@gmail.com>
2022-06-07 18:05:05 +0400

    feat: add Flask-Mail

--- app.py
+++ app.py
@@ -2,6 +2,7 @@ import config
 
 from flask import Flask
 from flask_migrate import Migrate
+from flask_mail import Mail
 from flask_sqlalchemy import SQLAlchemy
 from routes import auth
 
@@ -21,6 +22,19 @@ migrate = Migrate(app, db)
 # Endpoint registration
 app.register_blueprint(auth.bp)
 
+# SMTP Mail
+app.config.update({
+    "MAIL_SERVER": 'smtp.gmail.com',
+    "MAIL_DEBUG": False,
+    "MAIL_PORT": 465,
+    "MAIL_USE_TLS": False,
+    "MAIL_USE_SSL": True,
+    "MAIL_USERNAME": config.email_sender,
+    "MAIL_PASSWORD": config.email_pwd
+})
+
+mail = Mail()
+mail.init_app(app)
 
 if __name__ == '__main__':
     app.run()
--- config.py
+++ config.py
@@ -10,16 +10,18 @@ def check_if_env_variable_exists(variable):
         sys.exit(1)
 
 
-required_variables = ("DB", "FLASK_ENV", "FLASK_DEBUG")
+required_variables = ("DB", "FLASK_ENV", "FLASK_DEBUG", "SMTP_EMAIL_USER", "SMTP_EMAIL_PASSWORD")
+
 for env in required_variables:
     check_if_env_variable_exists(env)
 
 DB = os.environ.get("DB")
 SQLALCHEMY_DATABASE_URI = os.environ.get("SQLALCHEMY_DATABASE_URI")
 SQLALCHEMY_DATABASE_URI_LOCAL = os.environ.get("SQLALCHEMY_DATABASE_URI_LOCAL")
-FLASK_ENV = os.environ.get("FLASK_ENV")
-FLASK_DEBUG = os.environ.get("FLASK_DEBUG")
 
+# App config
 postgresConn = SQLALCHEMY_DATABASE_URI if DB == "cloud" else SQLALCHEMY_DATABASE_URI_LOCAL
-env = FLASK_ENV
-debug = FLASK_DEBUG
+env = os.environ.get("FLASK_ENV")
+debug = os.environ.get("FLASK_DEBUG")
+email_sender = os.environ.get("SMTP_EMAIL_USER")
+email_pwd = os.environ.get("SMTP_EMAIL_PASSWORD")
