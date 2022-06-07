5b8583cbb7c77c0a3bce09ba088f531cad302d94
Emmanuel SARPEDON <emmanuel.sarpedon@gmail.com>
2022-06-07 17:30:55 +0400

    chore (app.py): refactor Flask app instance config

--- app.py
+++ app.py
@@ -2,18 +2,25 @@ import config
 
 from flask import Flask
 from flask_migrate import Migrate
-from models.user import db
+from flask_sqlalchemy import SQLAlchemy
 from routes import auth
 
 app = Flask(__name__)
-app.config['SQLALCHEMY_DATABASE_URI'] = config.postgresConn
+
+# General configuration
 app.config['FLASK_ENV'] = config.env
 app.config['DEBUG'] = config.debug
-app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
 
-app.register_blueprint(auth.bp)
+# Database and model
+app.config['SQLALCHEMY_DATABASE_URI'] = config.postgresConn
+app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
+db = SQLAlchemy()
 db.init_app(app)
 migrate = Migrate(app, db)
+
+# Endpoint registration
+app.register_blueprint(auth.bp)
+
 
 if __name__ == '__main__':
     app.run()
